cae5f589641563c14ee3967653e4bd7115d0015c
Vitaliy <53655132+krasavchik1@users.noreply.github.com>
2021-05-15 16:42:22 +0300

    new model

--- models/user.js
+++ models/user.js
@@ -1,8 +1,31 @@
+
 exports.user = {
-    id: '',
-    firstName: '',
-    lastName: '',
-    email: '',
-    phoneNumber: '',
-    password: '' // min 3 symbols
+    "user_id": "3",
+    "user_login": "",
+    "is_root": "",
+    "timestamp": "1116709280",
+    "user_type": "C",
+    "status": "A",
+    "firstname": "",
+    "lastname": "",
+    "email": ""
+},
+    {
+        "user_id": "1",
+        "user_login": "admin",
+        "is_root": "Y",
+        "timestamp": "1112556008",
+        "user_type": "A",
+        "status": "A",
+        "firstname": "John",
+        "lastname": "Doe",
+        "email": "admin1gmail.com",
+        "company": "Your company",
+        "company_id": "0",
+        "company_name": null,
+        "Content-type": "text/plain",
+    },
+{
+    "user_id": 11,
+    "profile_id": "12"
 }
